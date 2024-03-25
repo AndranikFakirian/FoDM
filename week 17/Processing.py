459432d4d587cc5b83292465d20ae4ee3e15d243
Pchel <fakirian.av@phystech.edu>
2024-03-25 16:58:22 +0300

    Another week work

---
+++ week 17/Processing.py
@@ -0,0 +1,104 @@
+import json
+import subprocess as sub
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tic
+import matplotlib.animation as anim
+import scipy.optimize as so
+import time as t
+import math as m
+
+def Gauss(x, *p):
+    x0, sigma = p
+    return 1/(sigma*m.sqrt(2*np.pi))*np.exp(-(x-x0)**2/(2.*sigma**2))
+F_base='Offset'
+Inp_f=F_base+'.json'
+Outp_f=F_base+'_outp.json'
+PB=0.5
+NumSteps0=1
+NumSteps1=1000
+StepNumSteps=10
+NumStepss=range(NumSteps0, NumSteps1, StepNumSteps)
+Rep=1000
+'''AvC=[]
+AvModC=[]
+AvSqrC=[]'''
+JS={"R": Rep, "N0": NumSteps0, "N1": NumSteps1, "SN": StepNumSteps, "PB": PB}
+with open(Inp_f, 'w') as js:
+    json.dump(JS, js)
+sub.run(['RandWalks.exe', Inp_f, Outp_f])
+with open(Outp_f) as js:
+    Walks = json.load(js)
+Coordss=np.array(Walks["Coordss"])
+EndCoords=np.array(Coordss[-1])
+Hist, Bins = np.histogram(EndCoords, bins=range(-100, 100))
+EndCoordsMin=np.min(EndCoords)
+EndCoordsMax=np.max(EndCoords)
+DEndCoords=EndCoordsMax-EndCoordsMin
+AvModC=np.average(EndCoords)
+AvCErr=m.sqrt(np.average(np.square(EndCoords))-(np.average(EndCoords))**2)
+X=np.linspace(EndCoordsMin-int(DEndCoords*0.1), EndCoordsMax+int(DEndCoords*0.1), 10000)
+Y=1/(AvCErr*m.sqrt(np.pi*2))*np.exp(-np.square(X-AvModC)/(2*AvCErr**2))
+BC=(Bins[1:]+Bins[:-1])/2
+p0=[AvModC, AvCErr]
+coeff, var_matrix = so.curve_fit(Gauss, BC, Hist/(Rep), p0=p0)
+Hist_fit=Gauss(np.array(BC), *[i for i in coeff])
+fig, ax = plt.subplots()
+#ax.plot(Reps, AvCErr)
+ax.plot(X, Y)
+ax.stairs(Hist/(Rep*m.sqrt(np.pi*m.sqrt(2))), Bins)
+ax.plot(BC, Hist_fit)
+plt.show()
+'''for i in range(Shape[0]):
+    AvC.append(np.average(Coordss[i]))
+    AvModC.append(np.average(np.abs(Coordss[i])))
+    AvSqrC.append(np.average(np.square(Coordss[i])))
+Coordss=Coordss.T
+AvC=np.array(AvC)
+AvModC=np.array(AvModC)
+AvSqrC=np.array(AvSqrC)
+NumStepss=np.array(NumStepss)'''
+'''fig, ax = plt.subplots(3, 1)
+ax[0].scatter(NumStepss, AvC)
+ax[1].plot(NumStepss, AvModC)
+ax[2].scatter(NumStepss, AvSqrC)
+plt.show()'''
+'''fig, ax = plt.subplots()
+for i in range(Rep):
+    ax.scatter(NumStepss, Coordss[i])
+plt.show()'''
+'''P=10
+Voc=[1, 2]
+R=1000
+Phrs=[]
+for x in Voc:
+    for y in Voc:
+        Phrs.append([x, y])
+NPhrs=Normalize(Phrs)
+AvLenss=[]
+for i in range(P):
+    AvLens=[]
+    for Phr in Phrs:
+        JS={"R": R, "Voc": Voc, "Phr": Phr}
+        with open(Inp_f, 'w') as js:
+            json.dump(JS, js)
+        sub.run(['test.exe', Inp_f, Outp_f])
+        with open(Outp_f) as js:
+            al = json.load(js)
+        AvLens.append(al["AvLen"])
+    AvLenss.append(AvLens)
+    t.sleep(i/13)
+AvLenss=np.array(AvLenss)
+AvLensa=np.array([np.average(AvLenss.T[i]) for i in range(len(Phrs))])
+#print(AvLensa)
+AvLenserr=np.sqrt(abs(np.array([np.average(np.square(AvLenss.T[i])) for i in range(len(Phrs))])-np.square(AvLensa)))
+#print(AvLenserr)
+AvLensmin=AvLensa-AvLenserr
+AvLensmax=AvLensa+AvLenserr
+fig, ax=plt.subplots(1, 2)
+ax[0].plot(NPhrs, AvLensa)
+#ax[0].errorbar(Ns, PBsa, PBserr)
+ax[0].fill_between(NPhrs, AvLensmin, AvLensmax, alpha=0.5)
+#ax[0].plot(Ns, PBsan)
+ax[1].plot(NPhrs, AvLenserr)
+plt.show()'''
---
+++ week 17/RandWalks.cpp
@@ -0,0 +1,120 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <random>
+#include <string>
+#include <ctime>
+#include <nlohmann/json.hpp>
+#include <set>
+#include <windows.h>
+#include <iterator>
+using namespace std;
+using json=nlohmann::json;
+
+template <class R>
+void newSeed(R &r)
+{
+    auto T=time(nullptr);
+    r.seed(T);
+    ofstream sd("Seed.json");
+    json jsd={};
+    jsd["S"]=T;
+    sd<<jsd;
+    sd.close();
+}
+
+template <class R>
+void lastSeed(R &r)
+{
+    ifstream sd("Seed.json");
+    json jsd=json::parse(sd);
+    r.seed(jsd["S"]);
+}
+
+template <class R>
+int fS(vector <R> res, vector <R> phr)
+{
+    if (res.size()<phr.size())
+    {
+        return -1;
+    }
+    vector <R> chck;
+    int ps=phr.size();
+    int rs=res.size();
+    chck.resize(ps);
+    for (int i=0; i<ps; i++)
+    {
+        chck[i]=res[i+rs-ps];
+    }
+    if (chck==phr)
+    {
+        return rs;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int DiscrStep (float PB, float R)
+{
+    int S[2]={-1, 1};
+    if (R<PB)
+    {
+        return S[0];
+    }
+    else
+    {
+        return S[1];
+    }
+}
+int main(int argc, char* argv[])
+{
+    ifstream f1(argv[1]);
+    json js1=json::parse(f1);
+    //random_device r;
+    linear_congruential_engine<unsigned int, 48271, 0, 2147483647> r;
+    newSeed<linear_congruential_engine<unsigned int, 48271, 0, 2147483647>> (r);
+    //lastSeed<linear_congruential_engine<unsigned int, 48271, 0, 2147483647>> (r);
+    //cout<<time(nullptr)<<endl;
+    default_random_engine e1(r());
+    int NumSteps0=js1["N0"];
+    int NumSteps1=js1["N1"];
+    int StepNumSteps=js1["SN"];
+    int Rep=js1["R"];
+    float PB=js1["PB"];
+    vector <vector <int>> Coordss;
+    f1.close();
+    int NumSteps=NumSteps0;
+    int Num0Steps=0;
+    while (NumSteps<NumSteps1) {
+        vector <int> Coords;
+        for (int i = 0; i < Rep; i++) {
+            int Coord=0;
+            if (!Coordss.empty())
+            {
+                Coord=(*(Coordss.end()-1))[i];
+            }
+            for (int j = Num0Steps; j < NumSteps; j++) {
+                uniform_real_distribution<float> uniform_dist(0, 1);
+                float R = uniform_dist(e1);
+                Coord += DiscrStep(PB, R);
+            }
+            Coords.push_back(Coord);
+        }
+        Coords.shrink_to_fit();
+        Coordss.push_back(Coords);
+        Num0Steps=NumSteps;
+        NumSteps+=StepNumSteps;
+    }
+    Coordss.shrink_to_fit();
+    ofstream f2(argv[2]);
+    json js2={};
+    js2["Coordss"]=Coordss;
+    f2<<js2;
+    f2.close();
+
+    return 0;
+}
